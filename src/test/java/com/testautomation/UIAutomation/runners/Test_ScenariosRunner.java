f959cee8314a1f0c8754dc6ff739bcf95c6212a3
Alok <alok.sabran@gmail.com>
2020-11-23 03:14:58 +0530

    Remove DryRun Cucumber Option

--- src/test/java/com/testautomation/UIAutomation/runners/Test_ScenariosRunner.java
+++ src/test/java/com/testautomation/UIAutomation/runners/Test_ScenariosRunner.java
@@ -12,7 +12,6 @@ import io.cucumber.testng.CucumberOptions.SnippetType;
 		features = "classpath:features",
 		glue = "com.testautomation.UIAutomation",
 		tags = "@heroku", //or @webdriveruniversity1",
-		dryRun = true,
 		monochrome = true,
 		plugin = {"pretty", 
 				  //"html:reports/cucumber-report.html",
